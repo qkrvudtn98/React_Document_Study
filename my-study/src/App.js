669c72683aab337b16f216c59db5db9201eab0d8
qkrvudtn98 <ps3624@naver.com>
2022-04-25 05:20:19 +0900

    Copy Component and ref

--- my-study/src/App.js
+++ my-study/src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import IterationSample from './IterationSample';
+// import ScrollBox from './ScrollBox';
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from './Say';
 // import EventPractice from './EventPractice';
-import ValidationSample from './ValidationSample';
+// import ValidationSample from './ValidationSample';
 
-const App = () => {
-  return (
-    // <MyComponent name='종기가 난 작은 공작새' favoriteNumber={7}>
-    //   리액트
-    // </MyComponent>
+class App extends Component {
+  render() {
+    return (
+      // <MyComponent name='종기가 난 작은 공작새' favoriteNumber={7}>
+      //   리액트
+      // </MyComponent>
+      // <Counter></Counter>
+      // <Say />
+      // <EventPractice />
+      // <ValidationSample />
+  
+      // <div>
+      //   <ScrollBox  ref={(ref) => this.scrollBox=ref}/>
+      //   <button onClick={() => this.scrollBox.scrollToBottom()}>
+      //     맨 밑으로
+      //   </button>
+      // </div>
 
-    // <Counter></Counter>
-
-    // <Say />
-
-    // <EventPractice />
-
-    <ValidationSample />
-  );
-};
+      <IterationSample />
+    );
+  }
+}
 
 export default App;
