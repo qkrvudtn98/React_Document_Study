f8f501c786a124690d72f71b03c17c55cb2f431a
qkrvudtn98 <ps3624@naver.com>
2022-04-26 01:57:45 +0900

    Rendering

--- my-study/src/App.js
+++ my-study/src/App.js
@@ -9,7 +9,21 @@ import LifeCycleSample from './LifeCycleSample';
 // import EventPractice from './EventPractice';
 // import ValidationSample from './ValidationSample';
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       // <MyComponent name='종기가 난 작은 공작새' favoriteNumber={7}>
@@ -29,7 +43,10 @@ class App extends Component {
 
       // <IterationSample />
 
-      <LifeCycleSample />
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
     );
   }
 }
